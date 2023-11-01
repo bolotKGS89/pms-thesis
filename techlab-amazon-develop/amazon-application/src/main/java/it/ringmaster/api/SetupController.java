85429580c78be62cc10d47143961018ee323fd45
Bolot <bolot.89@gmail.com>
2023-11-01 22:40:40 +0100

    done

--- techlab-amazon-develop/amazon-application/src/main/java/it/ringmaster/api/SetupController.java
+++ techlab-amazon-develop/amazon-application/src/main/java/it/ringmaster/api/SetupController.java
@@ -1,8 +1,12 @@
 package it.ringmaster.api;
 
 
+import it.ringmaster.PaymentDto;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/pms/amazon-pay")
 @AllArgsConstructor
 public class SetupController {
+
+    @PostMapping(path = "/setup", produces = MediaType.APPLICATION_JSON_VALUE)
+    public PaymentDto create(@RequestBody PaymentDto paymentDto) {
+        return null;
+    }
 }
--- techlab-amazon-develop/amazon-application/src/main/java/it/ringmaster/api/WebhookController.java
+++ techlab-amazon-develop/amazon-application/src/main/java/it/ringmaster/api/WebhookController.java
@@ -4,7 +4,7 @@ import it.ringmaster.WebhookDto;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class WebhookController {
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public WebhookDto getWebhook() {
         return null;
     }
