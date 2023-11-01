b5e7d79a7ecaf759e5ee808126f1517ba92a92c1
Bolot <bolot.89@gmail.com>
2023-11-01 20:41:03 +0100

    docker compose is ready

---
+++ techlab-pms-develop/pms-application/src/main/java/it/ringmaster/PmsServerApplication.java
@@ -0,0 +1,13 @@
+package it.ringmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmsServerApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(PmsServerApplication.class, args);
+    }
+}
